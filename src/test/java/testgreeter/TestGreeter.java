d155fa1496c03e38abd624468019bd18f725e95c
Ubuntu <ubuntu@ip-172-31-1-93.ec2.internal>
2016-05-11 17:57:51 +0000

    Testing webhook16

--- src/test/java/testgreeter/TestGreeter.java
+++ src/test/java/testgreeter/TestGreeter.java
@@ -8,9 +8,16 @@ import hello.Greeter;
 public class TestGreeter {
 
   @Test
-  public void testGreeterOutput() {
+  public void testGreeterOutputHello() {
     Greeter greeter = new Greeter();
     String val = greeter.sayHello();
     Assert.assertEquals("Hello world!", val);
+  }
+
+  @Test
+  public void testGreeterOutputGoodbye() {
+    Greeter greeter = new Greeter();
+    String val = greeter.sayGoodbye();
+    Assert.assertEquals("Goodbye world!", val);
   }
 }
