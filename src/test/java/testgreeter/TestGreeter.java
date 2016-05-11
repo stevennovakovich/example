db84d89637d23bea273e3395e2dd8733efd50811
Ubuntu <ubuntu@ip-172-31-1-93.ec2.internal>
2016-05-11 01:03:59 +0000

    Testing webhook12

--- src/test/java/testgreeter/TestGreeter.java
+++ src/test/java/testgreeter/TestGreeter.java
@@ -1,12 +1,14 @@
-package testhello;
+package testgreeter;
 
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import hello.Greeter;
 
-public class TestHelloWorld {
-  public void testHelloWorld() {
+public class TestGreeter {
+
+  @Test
+  public void testGreeterOutput() {
     Greeter greeter = new Greeter();
     String val = greeter.sayHello();
     Assert.assertEquals("Hello world!", val);
