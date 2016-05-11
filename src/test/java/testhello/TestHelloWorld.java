0929537e73d2b12cf9b2fbeec4ebb082f9cdfadc
Ubuntu <ubuntu@ip-172-31-1-93.ec2.internal>
2016-05-11 00:26:22 +0000

    Testing webhook7

--- src/test/java/testhello/TestHelloWorld.java
+++ src/test/java/testhello/TestHelloWorld.java
@@ -5,5 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestHelloWorld {
-
+  @Test
+  public void testHelloWorld() {
+    Greeter greeter = new Greeter();
+    String val = greeter.sayHello();
+    Assert.assertEquals("Hello world!", val);
+  }
 }
